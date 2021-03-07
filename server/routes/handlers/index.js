a5d1adc7ea334404d9e17b03b5de7b9fe7de6d01
Alex Park <alexparkvinyas@gmail.com>
2021-03-07 20:37:10 +0100

    server - add deleteCard service, route, tests

--- server/routes/handlers/index.js
+++ server/routes/handlers/index.js
@@ -2,4 +2,5 @@ module.exports = {
     createCardHandler: require('./createCardHandler'),
     retrieveCardsHandler: require('./retrieveCardsHandler'),
     updateCardHandler: require('./updateCardHandler'),
+    deleteCardHandler: require('./deleteCardHandler')
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,10 @@
 const { Router } = require('express');
-const { createCardHandler, retrieveCardsHandler, updateCardHandler } = require('./handlers');
+const {
+    createCardHandler,
+    retrieveCardsHandler,
+    updateCardHandler,
+    deleteCardHandler
+} = require('./handlers');
 const { json } = require('body-parser');
 
 const router = new Router();
@@ -9,5 +14,6 @@ const bodyParser = json();
 router.post('/card', bodyParser, createCardHandler);
 router.get('/cards', retrieveCardsHandler);
 router.patch('/card/:cardId', bodyParser, updateCardHandler);
+router.delete('/card/:cardId', deleteCardHandler);
 
 module.exports = { router };
