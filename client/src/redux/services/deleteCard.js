013d57a889a69ffe5f1a03c42d601ea0f87ce2f6
Alex Park <alexparkvinyas@gmail.com>
2021-03-14 19:27:37 +0100

    app - add env on client, fix logger on server

--- client/src/redux/services/deleteCard.js
+++ client/src/redux/services/deleteCard.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
+const PORT = process.env.REACT_APP_PORT;
+const DOMAIN = process.env.REACT_APP_DOMAIN;
 
 export const deleteCard = async (cardId) => {
 	try {
-		await axios.delete(`http://localhost:8085/api/card/${cardId}`);
+		await axios.delete(`http://${DOMAIN}:${PORT}/api/card/${cardId}`);
 	} catch (error) {
 		throw error;
 	}
--- client/src/redux/services/retrieveCards.js
+++ client/src/redux/services/retrieveCards.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
+const PORT = process.env.REACT_APP_PORT;
+const DOMAIN = process.env.REACT_APP_DOMAIN;
 
 export const retrieveCards = async () => {
 	try {
-		const cards = await axios.get('http://localhost:8085/api/cards');
+		const cards = await axios.get(`http://${DOMAIN}:${PORT}/api/cards`);
 		return cards;
 	} catch (error) {
 		throw error;
